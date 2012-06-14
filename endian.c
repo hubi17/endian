1d8bda31acbbed2149c653622c00ce4a05811507
Hubi <constantine17@gmail.com>
2012-06-14 22:07:08 +0200

    initial commit

---
+++ endian.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main( void ) {
+	long int i = 2 * (long int) pow( 2, 24 ) + 3 * 256 + 2;
+	const char *p = (const char *) &i;
+
+	if( p[0] == 1 )
+		printf( "result: LITTLE ENDIAN\n" );
+	else
+		printf( "result: BIG ENDIAN\n" );
+
+	printf( "%s - %d\n", "p[0]", (long int) p[0] );
+	printf( "%s - %d\n", "p[1]", (long int) p[1] );
+	printf( "%s - %d\n", "p[2]", (long int) p[2] );
+	printf( "%s - %d\n", "p[3]", (long int) p[3] );
+	
+	return 0;
+}
